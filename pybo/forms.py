896dd40c7a6aa68da4d3d95ed87d711fd0fe866a
jaeyunim <ijy00120877@gmail.com>
2022-08-22 19:42:58 +0900

    add security

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,5 +1,7 @@
+from dataclasses import fields
+from pyexpat import model
 from django import forms
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 
 class QuestionForm(forms.ModelForm):
@@ -17,4 +19,13 @@ class QuestionForm(forms.ModelForm):
         labels = {
             "subject": "제목",
             "content": "내용",
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ["content"]
+        labels = {
+            "content": "답변내용",
         }
--- pybo/views.py
+++ pybo/views.py
@@ -2,7 +2,7 @@ from http.client import HTTPResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 # Create your views here.
 def index(request):
@@ -28,10 +28,18 @@ def answer_create(request, question_id):
     """pybo 답변 등록"""
 
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(
-        content=request.POST.get("content"), create_date=timezone.now()
-    )
-    return redirect("pybo:detail", question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect("pybo:detail", question_id=question.id)
+        else:
+            form = AnswerForm()
+        context = {"question": question, "form": form}
+        return render(request, "pybo/question_detail.html", context)
 
 
 def question_create(request):
@@ -47,5 +55,5 @@ def question_create(request):
             return redirect("pybo:index")
     else:
         form = QuestionForm()
-    contenxt = {"form": form}
-    return render(request, "pybo/question_form.html", contenxt)
+    context = {"form": form}
+    return render(request, "pybo/question_form.html", context)
